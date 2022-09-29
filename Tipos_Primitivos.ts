d3e487086c936662582ffaa5f5eaf344b0afba48
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-29 17:54:24 -0300

    Create Tipos_Primitivos.ts

---
+++ Tipos_Primitivos.ts
@@ -0,0 +1,11 @@
+// Esses são os tipos primitivos que podemos utilizar no TypeScript:
+
+let loading: boolean;
+loading = false;
+
+let email: string;
+email = "joao@email.com";
+
+let price: number;
+price = 10.50;
+price = 10;
