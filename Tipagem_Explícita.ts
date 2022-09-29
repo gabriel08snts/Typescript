1735df80ffc25c5a33ad6bf7d103bd935bf03057
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-29 17:33:56 -0300

    Delete Tipagem_Explícita.ts

--- Tipagem_Explícita.ts
+++
@@ -1,5 +0,0 @@
-\\ Na tipagem explícita nós especificamos o qual será o tipo de cada variável.
-
-user: string, ticket: number;
-
-\\ O TypeScript nos ajuda a identificar cada tipo de variável ao invocá-las.
