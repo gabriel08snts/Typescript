75bb5b429fa65267ecb6e6a08b1b3b009820b2ea
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-29 19:14:14 -0300

    Update Inferência_de_Tipos.ts

--- Inferência_de_Tipos.ts
+++ Inferência_de_Tipos.ts
@@ -1,8 +1,8 @@
 // O TypeScript reconhece automaticamente o tipo da variável ao passar algum valor para ela.
 
-let user = "João"
+let user = "João";
 
-user = 10
+user = 10;
 
 /* (O typescript avisara no exemplo acima que isso é um erro porque ele ja identificou anteriormente que a variável "user" é do tipo "String", 
 não podendo redeclarar ela mudando seu tipo) */
