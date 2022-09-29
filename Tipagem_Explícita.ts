99b53421363f4c0d543c588910259a92e6e80941
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-29 17:34:28 -0300

    Commit Sobre Tipagem

---
+++ Tipagem_Explícita.ts
@@ -0,0 +1,6 @@
+// Na tipagem explícita nós especificamos o qual será o tipo de cada variável.
+
+let user: string;
+let ticket: number;
+
+// O TypeScript nos ajuda a identificar cada tipo de variável ao invocá-las.
