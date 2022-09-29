c66af119be7eeb0ef007ca6430b877d41c098e29
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-29 19:12:22 -0300

    Create Arrays.ts

---
+++ Arrays.ts
@@ -0,0 +1,7 @@
+// Existem duas maneiras de tipar arrays no TypeScript, confira os exemplos:
+
+let numbers: number[];
+numbers = [1,2,3,4,5,6];
+
+let users: Array<string>;
+users = ['Rodrigo', 'João'];
