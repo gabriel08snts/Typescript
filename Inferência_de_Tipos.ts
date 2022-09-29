d9621f6c3177dc96df1cb6023249e116b7e800d1
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-29 17:49:48 -0300

    Update Inferência_de_Tipos.ts

--- Inferência_de_Tipos.ts
+++ Inferência_de_Tipos.ts
@@ -2,4 +2,7 @@
 
 let user = "João"
 
-// user = 10 (O typescript avisara que isso é um erro porque ele ja identificou anteriormente que a variável é do tipo "String", não podendo redeclarar ela mudando seu tipo)
+user = 10
+
+/* (O typescript avisara no exemplo acima que isso é um erro porque ele ja identificou anteriormente que a variável "user" é do tipo "String", 
+não podendo redeclarar ela mudando seu tipo) */
