2f50aee7b4fa2acfe3278b06fe8b92491d4412b3
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-29 17:37:48 -0300

    Variável Any

---
+++ Any.ts
@@ -0,0 +1,12 @@
+/* Ao declarar uma variável no TypeScript o seu tipo por padrão será any. Mas devemos tomar um certo cuidado com essa tipagem porque ela pode ser perigosa ao decorrer do nosso projeto, não é muito recomendada a sua utilização. */
+
+// Exemplo:
+
+let info: any;
+
+info = [123];
+info = 'João';
+info = true;
+info = 10.50;
+
+// Com o tipo any a variável aceitará qualquer valor.
