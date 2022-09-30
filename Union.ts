52b485fb7c7bde9ec04ecb92cea5c14b673937d6
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-30 14:45:21 -0300

    Create Union.ts

---
+++ Union.ts
@@ -0,0 +1,10 @@
+// O Operador Union, representado pelo pipe | nos permite adicionar mais de um tipo na variável. Vamos ao exemplo:
+
+function printUserId(id: number | string) {
+    console.log(`O ID do usuário é: ${id}`);
+}
+
+printUserId(101);
+printUserId("101");
+
+// Dessa forma, podemos passar tanto number quanto string.
