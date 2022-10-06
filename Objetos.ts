f954577c64d64a3530d7b92e384acfb0e0fe0e16
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-10-06 15:14:33 -0300

    Create Objetos.ts

---
+++ Objetos.ts
@@ -0,0 +1,21 @@
+// Criando tipagens para/utilizando objetos no TypeScript.
+
+// Segue o exemplo:
+
+type Point = {
+    x: number;
+    y: number;
+}
+
+function printCoord(points: Point) {
+    console.log(`O eixo x é: ${points.x}`)
+    console.log(`O eixo y é: ${points.y}`)
+}
+
+printCoord({x: 101, y: 50})
+
+// Resultado do log:
+
+[LOG]: "O eixo x é: 101"
+------------------------
+[LOG]: "O eixo y é: 50"
