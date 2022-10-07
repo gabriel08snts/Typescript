9d91b9a2a517a26808fc266cdbd98f0480e35274
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-10-06 23:21:27 -0300

    Create TSconfig.ts

---
+++ TSconfig.ts
@@ -0,0 +1,28 @@
+// É um arquivo de configuração do TypeScript, ele pode ser escrito tanto em formato de JavaScript quanto no formato JSON.
+
+// Exemplo de um arquivo tsconfig.json :
+
+{
+  "compilerOptions": {
+    "module": "commonjs",
+    "noImplicitAny": true,
+    "removeComments": true,
+    "preserveConstEnums": true,
+    "sourceMap": true
+  },
+  "files": [
+    "core.ts",
+    "sys.ts",
+    "types.ts",
+    "scanner.ts",
+    "parser.ts",
+    "utilities.ts",
+    "binder.ts",
+    "checker.ts",
+    "emitter.ts",
+    "program.ts",
+    "commandLineParser.ts",
+    "tsc.ts",
+    "diagnosticInformationMap.generated.ts"
+  ]
+}
